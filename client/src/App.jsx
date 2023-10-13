3cf715b47b77750ea4dba845493b8c3c205c8608
Yodae <yodae.lim@gmail.com>
2023-10-13 14:37:00 -0700

    create vite project, set up tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <div class="text-xl font-medium text-red-600">Recigo</div>
+        <p class="text-slate-500">You have a new recipe!</p>
+      </div>
+    </>
+  );
+}
+
+export default App
