0a5f29824dc33537bc7062eca484e4e4a85b4fbe
Tomomi-K1 <tomomi.music@gmail.com>
2023-10-24 15:16:44 -0700

    added recipeList

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import UserLoginPage from "./pages/UserLoginPage";
-import SearchBox from './components/SearchBox';
+import Explore from "./pages/Explore";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<UserLoginPage />} />
-        <Route path="/explore" element={<SearchBox />} />
+        <Route path="/explore" element={<Explore />} />
       </Routes>
     </Router>
   );
