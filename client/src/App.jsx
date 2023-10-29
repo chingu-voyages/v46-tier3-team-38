6ec1efe195b0d03b0a3adf65d43b04aed4f538fa
MS07062000 <singhmurali.14@gmail.com>
2023-10-29 08:57:56 +0530

    removed login related component and pages , route from this branch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,11 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import UserLoginPage from "./pages/UserLoginPage";
 import ViewRecipeDetails from "./pages/viewRecipeDetails";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<UserLoginPage />} />
         <Route path="/viewRecipeDetails/:recipeID" element={<ViewRecipeDetails />} />
       </Routes>
     </Router>
--- client/src/pages/UserLoginPage.jsx
+++
@@ -1,7 +0,0 @@
-import UserLogin from "../components/userLogin";
-
-function UserLoginPage() {
-  return <UserLogin />;
-}
-
-export default UserLoginPage;
