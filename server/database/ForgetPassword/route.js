d52323dd6b1f363d07323abadc0d0b5a3c32ea63
MS07062000 <singhmurali.14@gmail.com>
2023-11-01 09:30:09 +0530

    Modified routes so that it acknowledges the request by sending true if sucess else by sending error

--- server/database/ForgetPassword/route.js
+++ server/database/ForgetPassword/route.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 router.post('/forgetPassword', async (req, res) => {
     try {
         const email=req.body.email;
-        res.sendStatus(200);
+        res.status(200).send(true);
     } catch (error) {
         res.status(400).send(error);
     }
