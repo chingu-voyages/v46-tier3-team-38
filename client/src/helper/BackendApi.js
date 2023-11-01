687ec39672591a3dc9ee06ba6fec6ee47841ba69
MS07062000 <singhmurali.14@gmail.com>
2023-11-01 09:35:03 +0530

    created two post request one for signup and one for login

--- client/src/helper/BackendApi.js
+++ client/src/helper/BackendApi.js
@@ -7,6 +7,24 @@ class backendAPI{
         let response = await axios.get(`${BASE_URL}/search/?q=${term}`);
         return response.data
     }
+
+    static async signUp(username,pwd){
+        const data={
+            username:username,
+            pwd:pwd
+        }
+        const response = await axios.post(`${BASE_URL}/signUp`,data);
+        return response.data
+    }
+
+    static async login(username,pwd){
+        const data={
+            username:username,
+            pwd:pwd
+        }
+        const response = await axios.post(`${BASE_URL}/login`,data);
+        return response.data
+    }
 }
 
 
