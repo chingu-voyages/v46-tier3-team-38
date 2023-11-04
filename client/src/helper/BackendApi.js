28bccae50a980a5546ad16b34a25cab224d979a4
MS07062000 <singhmurali.14@gmail.com>
2023-11-04 09:28:05 +0530

    Made some fixes in backend api

--- client/src/helper/BackendApi.js
+++ client/src/helper/BackendApi.js
@@ -8,16 +8,18 @@ const BASE_URL = ENVIRONMENT ==='development' ? "http://localhost:3000": import.
 
 class BackendAPI{
     static async searchRecipe(term){
-        let response = await axios.get(`${BASE_URL}/search/?q=${term}`);
+        //console.log(BASE_URL)
+        const response = await axios.get(`${BASE_URL}/search/?q=${term}`);
         return response.data
     }
     
     static async getRandomRecipe(){
-      let response = await axios.get(`${BASE_URL}/random`);
-      return response.data
+        const response = await axios.get(`${BASE_URL}/random`);
+        return response.data
     } 
+    
     static async getRecipeDetail(recipeID) {
-        const recipeDetail = await axios.get(`http://localhost:3000/recipeInfo/${recipeID}`, { 'Origin': 'http://localhost:5173/' });
+        const recipeDetail = await axios.get(`${BASE_URL}/recipeInfo/${recipeID}`);
         const recipe = recipeDetail.data.recipe;
         // console.log(recipe);
         return recipe;
