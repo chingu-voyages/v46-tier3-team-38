b43cd88da43ac147227b6d0b3758cccbecaaa522
Tomomi-K1 <tomomi.music@gmail.com>
2023-11-09 18:20:02 -0800

    removed Search Result

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import UserLoginPage from "./pages/UserLoginPage";
 import Explore from "./pages/Explore";
-import SearchResult from "./pages/SearchResult";
 import Navbar from "./components/Navbar";
 import ViewRecipeDetails from "./pages/viewRecipeDetails";
 
@@ -14,7 +13,6 @@ function App() {
       <Routes>
         <Route path="/" element={<UserLoginPage />} />
         <Route path="/explore" element={<Explore />} />
-        <Route path="/result/:term" element={<SearchResult />} />
         <Route path="/viewRecipeDetails/:recipeID" element={<ViewRecipeDetails />} />
       </Routes>
     </Router>
