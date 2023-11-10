4aa8bc6ecf243a2ebaf6690a8d709752818065b0
MS07062000 <singhmurali.14@gmail.com>
2023-11-10 21:44:23 +0530

    removed nnecessary route searchresult

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Explore from "./pages/Explore";
-import SearchResult from "./pages/SearchResult";
 import SignUp from "./pages/SignUp";
 import ForgetPassword from "./pages/ForgetPassword";
 import DashboardPage from "./pages/DashboardPage";
@@ -21,7 +20,6 @@ function App() {
           <Route path="/forgetPassword" exact element={<ForgetPassword />} />
           <Route path="/dashboard" exact element={<DashboardPage />} />
           <Route path="/explore" exact element={<Explore />} />
-          <Route path="/result/:term" exact element={<SearchResult />} />
           <Route path="/viewRecipeDetails/:recipeID" element={<ViewRecipeDetails />} />
         </Routes>
       </AuthProvider>
